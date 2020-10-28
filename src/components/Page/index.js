f17d4ec2d0695273c4d99c0faacf2a940d929d06
kabartolo <adairkate@gmail.com>
2020-10-28 19:53:33 +0800

    Move pageContext to Page prop list

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Page({ data }, pageContext) {
+export default function Page({ data, pageContext }) {
   return (
     <div>
       <pre>{JSON.stringify(data, null, 2)}</pre>
